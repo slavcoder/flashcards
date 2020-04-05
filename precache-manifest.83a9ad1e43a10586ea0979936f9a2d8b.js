96042ebfa1bd0ca31b6865f74fe21434ab251555
slavcoder <slawomircoder@gmail.com>
2020-04-05 23:01:08 +0200

    Updates

---
+++ precache-manifest.83a9ad1e43a10586ea0979936f9a2d8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b4f3a64f35bd15f8c2890bf1f2ed47",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "0d2ec0a8f7f866bb557f",
+    "url": "/flashcards/static/css/main.85696077.chunk.css"
+  },
+  {
+    "revision": "101bbc7da11a5fd507de",
+    "url": "/flashcards/static/js/2.87d16b3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flashcards/static/js/2.87d16b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d2ec0a8f7f866bb557f",
+    "url": "/flashcards/static/js/main.421479d7.chunk.js"
+  },
+  {
+    "revision": "0f769c985f487727dcfd",
+    "url": "/flashcards/static/js/runtime-main.dff91a57.js"
+  }
+]);
